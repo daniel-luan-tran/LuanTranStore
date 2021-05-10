bba2b15387c748ab18c4d12bc8bbdf510f3b65cb
Daniel Luan Tran <daniel.luan.tran@gmail.com>
2021-05-11 00:28:28 +0700

    Add database table

--- CategoryForm.cs
+++ CategoryForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace LuanTranStore
 {
@@ -15,6 +16,19 @@ namespace LuanTranStore
         public CategoryForm()
         {
             InitializeComponent();
+        }
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\UserDLT\Documents\smarketdb.mdf;Integrated Security=True;Connect Timeout=30");
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Con.Open();
+                string query = "Insert int CategoryTbl values("+CatIdTb.Text+""
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
